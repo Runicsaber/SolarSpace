c512d14856996da9517ee10218f78e37f497245f
Sulo Siigur <sulo.siigur@gmail.com>
2024-06-05 15:37:09 +0100

    added: basic features to main.js

---
+++ main.js
@@ -0,0 +1,22 @@
+/* 
+3 things always 
+
+scene
+camera
+renderer
+*/
+
+import * as three from 'three';
+
+//setting up the 3 main 'components'
+const scene = new three.Scene();
+const camera = new three.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+const renderer = new three.WebGLRenderer({
+  canvas: document.querySelector('#bg')
+});
+
+renderer.setPixelRatio(window.devicePixelRatio);
+renderer.setSize(window.innerWidth, window.innerHeight);
+camera.position.setZ(30);
+
+renderer.render(scene, camera); //render scene and camera
